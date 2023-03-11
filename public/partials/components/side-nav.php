5a05358ad9595d432a9a496784530535257d1b46
marlowe cheng <114950923+marlowecheng@users.noreply.github.com>
2023-03-11 14:55:10 -0800

    set up side nav, footer, & made login page

---
+++ public/partials/components/side-nav.php
@@ -0,0 +1,12 @@
+<nav class="">
+    <!-- profile pic -->
+    <a href=""><img src="" alt=""></a>
+    <ul class="">
+        <li class=""><a href="">Home</a></li>
+        <li class=""><a href="">Inbox</a></li>
+        <li class=""><a href="">Calendar</a></li>
+        <li class=""><a href="">My Project</a></li>
+        <li class=""><a href="">My Community</a></li>
+        <li class=""><a href="">More</a></li> 
+    </ul>
+</nav>
--- public/partials/global/footer.php
+++ public/partials/global/footer.php
@@ -1,3 +1,11 @@
 <footer>
+    <div>
+        <img src="" alt="forward logo">
+        <ul>
+            <li>Home</li>
+            <li>Inbox</li>
+            <li>Calendar</li>
+        </ul>
+    </div>
     <p>&copy; Forward 2023</p>
 </footer>
