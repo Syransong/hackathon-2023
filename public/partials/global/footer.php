df3d00ba8e07db9852c4220ff03f2c42164c1b10
Karina Song <ksysong@gmail.com>
2023-03-11 15:07:58 -0800

    started some preliminary styling and code for footer

--- public/partials/global/footer.php
+++ public/partials/global/footer.php
@@ -1,3 +1,23 @@
-<footer>
-    <p>&copy; Forward 2023</p>
+<footer class="d-flex">
+    <div class="d-flex footer">
+        <a href="<?php echo get_public_url('/index.php'); ?>" title="Home">
+            <img src="" alt="">
+            Forward
+        </a>
+        </a>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">FAQ</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <p>&copy; Forward 2023 | All Rights Reserved</p>
 </footer>
