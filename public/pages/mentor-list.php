55ec1d3e0644df2290d7e1c88d7c90d145e0f027
Karina Song <ksysong@gmail.com>
2023-03-11 12:06:36 -0800

    update paths on pages

--- public/pages/mentor-list.php
+++ public/pages/mentor-list.php
@@ -3,17 +3,17 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <?php require(get_project_path('public/partials/global/head.php'));  ?> 
+    <?php require(get_path('public/partials/global/head.php'));  ?> 
     <body>
         <?php 
-            include('partials/components/global/header.php');
+            require(get_path('public/partials/global/header.php'));
         ?>
         <main>
             <h1>Forward</h1>
             <p>The place to connect with local mentors </p>
         </main>
         <?php 
-            include('partials/components/global/footer.php');
+            require(get_path('public/partials/global/footer.php'));
         ?>
     </body>
 </html>
