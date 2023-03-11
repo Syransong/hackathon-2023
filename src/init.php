41d299283f31658ed925dbb4668e219946ce8228
Rikki Soriano <rikkilanz@gmail.com>
2023-03-11 10:01:58 -0800

    file structure

---
+++ src/init.php
@@ -0,0 +1,13 @@
+<?php
+
+    define('WWW_ROOT', 'http://localhost');
+    define('PROJECT_ROOT', dirname(__DIR__, 1));
+
+    //Functions
+    require('functions.php');
+
+    //Classes
+    require('Classes/User.php');
+    // require('data/projects.php');
+
+?>
