9e2d96a7fcd34eda7b9d7bd7695fbc651be6fb70
Karina Song <ksysong@gmail.com>
2023-03-11 12:06:14 -0800

    fix paths

---
+++ public/pages/project-showcase.php
@@ -0,0 +1,19 @@
+<?php
+    require('../src/init.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <?php require(get_project_path('public/partials/global/head.php'));  ?> 
+    <body>
+        <?php 
+            include('partials/components/global/header.php');
+        ?>
+        <main>
+            <h1>Forward</h1>
+            <p>The place to connect with local mentors </p>
+        </main>
+        <?php 
+            include('partials/components/global/footer.php');
+        ?>
+    </body>
+</html>
--- public/partials/global/header.php
+++ public/partials/global/header.php
@@ -4,7 +4,7 @@
     </a>
     <nav>
         <ul class="">
-            <li><a href="<?php echo get_public_url('/pages/mentor-list.php'); ?>" title="See all Mentors">>Mentors</a></li>
+            <li><a href="<?php echo get_public_url('/pages/mentor-list.php'); ?>" title="See all Mentors">Mentors</a></li>
             <li><a href="<?php echo get_public_url('/pages/project-showcase.php'); ?>" title="See Project Showcase">Project Showcase</a></li>
             <li><a href="<?php echo get_public_url('/pages/profile-page.php'); ?>" title="View User Profile">Profile</a></li>
         </ul>
