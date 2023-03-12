05f056ef56803fd51d700b63725ed65033349484
Rikki Soriano <rikkilanz@gmail.com>
2023-03-12 01:55:35 -0800

    added side nav on mentor

--- public/pages/mentor-list.php
+++ public/pages/mentor-list.php
@@ -8,30 +8,35 @@
         <?php 
             require(get_path('public/partials/global/header.php'));
         ?>
-        <main>
-            <section>
-                <h1>Mentor List</h1>
-            </section>
-            <section>
-                <div class="mentor-bar-wrapper">
-                    <div>
-                        <input type="text" placeholder="Search by role, job, language">
-                    </div>
-                    <div class="d-flex filter-btn">
-                        <div class="mentor-filter">
-                            <p>Specialization</p>
+        <main class="grid grid-row">
+            <?php
+                require(get_path('public/partials/components/side-nav.php'));
+            ?>
+            <section class="mentor-list">
+                <section>
+                    <h1>Mentor List</h1>
+                </section>
+                <section>
+                    <div class="mentor-bar-wrapper">
+                        <div>
+                            <input type="text" placeholder="Search by role, job, language">
                         </div>
-                        <div class="mentor-filter">
-                            <p>Filter</p>
+                        <div class="d-flex filter-btn">
+                            <div class="mentor-filter">
+                                <p>Specialization</p>
+                            </div>
+                            <div class="mentor-filter">
+                                <p>Filter</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            <section>
-                <div class="all-mentors">
-                    <?php foreach($mentors as $mentor): ?>
-                        <?php include(get_path('public/partials/components/mentor-card.php')); ?>
-                    <?php endforeach; ?>
-                </div>
+                <section>
+                    <div class="all-mentors">
+                        <?php foreach($mentors as $mentor): ?>
+                            <?php include(get_path('public/partials/components/mentor-card.php')); ?>
+                        <?php endforeach; ?>
+                    </div>
+                </section>
             </section>
         </main>
         <?php 
