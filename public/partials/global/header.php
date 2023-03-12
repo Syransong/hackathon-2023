292d5a9b2ae56c6b52aa46eba8b0ccedf3e55f63
marlowe cheng <114950923+marlowecheng@users.noreply.github.com>
2023-03-11 17:12:44 -0800

    changed header buttons

--- public/partials/global/header.php
+++ public/partials/global/header.php
@@ -4,8 +4,9 @@
     </a>
     <nav>
         <ul class="global-nav">
-            <li><a href="<?php echo get_public_url('/pages/mentor-list.php'); ?>" title="See all Mentors">Mentors</a></li>
-            <li><a href="<?php echo get_public_url('/pages/project-showcase.php'); ?>" title="See Project Showcase">Project Showcase</a></li>
+            <li><a href="<?php echo get_public_url('/pages/mentor-list.php'); ?>" title="See all Mentors">Community Hub</a></li>
+            <li><a href="<?php echo get_public_url('/pages/project-showcase.php'); ?>" title="See Project Showcase">Mentors</a></li>
+            <li><a href="<?php echo get_public_url('/pages/project-showcase.php'); ?>" title="See Project Showcase">Notifications</a></li>
             <li>
                 <div class="img-container">
                 <a href="<?php echo get_public_url('/pages/profile-page.php'); ?>" title="View User Profile"></a>
