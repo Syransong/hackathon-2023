dca1538694539fb65e730cf8bb064e245eb27586
Rikki Soriano <rikkilanz@gmail.com>
2023-03-12 01:42:54 -0800

    projects page styling

--- public/partials/components/project-card.php
+++ public/partials/components/project-card.php
@@ -1,14 +1,14 @@
 <div class="project-card">
     <img src="<?php echo get_public_url('/images/') . $project->image; ?>" alt="Mentor Profile Picture">
     <div class="mentor-info">
-        <ul class="d-flex">
+        <h2 class="project-name"><?php echo h($project->name); ?></h2>
+        <p class="project-author"><?php echo h($project->author_name); ?></p>
+        <ul class="d-flex project-tags">
             <?php 
                 foreach ($project->tags as $tag) {
-                    echo '<li>' . $tag . '</li>';
+                    echo '<li class="project-tag">' . $tag . '</li>';
                 }
             ?> 
         </ul>
-        <h2><?php echo h($project->name); ?></h2>
-        <p><?php echo h($project->author_name); ?></p>
     </div>
 </div>
