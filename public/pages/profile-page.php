280540a861d2eda9eb1df2aded669ac3c5e579b4
Karina Song <ksysong@gmail.com>
2023-03-11 18:42:47 -0800

    add content to profile page

--- public/pages/profile-page.php
+++ public/pages/profile-page.php
@@ -9,8 +9,23 @@
             require(get_path('public/partials/global/header.php'));
         ?>
         <main>
-            <h1>Your Profile</h1>
-            <p>See your communities and pending applications</p>
+            <h1>Hiya, Dave!</h1>
+            <section class="profile-card">
+                <div>
+                    <h2>Let's Find a Mentor</h2>
+                    <p>Take our survey and we'll help you find a compatible mentor near you</p>
+                    <a href="#">Go to Survey &rarr;</a>
+                </div>
+                <img src="" alt="">
+            </section>
+            <section class="profile-card">
+                <div>
+                    <h2>Find a Community!</h2>
+                    <p>Join a community and work together on your goals.</p>
+                    <a href="#">Go to Community Hub &rarr;</a>
+                </div>
+                <img src="" alt="">
+            </section>
         </main>
         <?php 
             require(get_path('public/partials/global/footer.php'));
