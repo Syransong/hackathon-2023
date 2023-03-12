89df53b5ef10135196f424bad1fcc343ab54cbf9
Karina Song <ksysong@gmail.com>
2023-03-11 20:15:21 -0800

    add styling and content to results

--- public/pages/results.php
+++ public/pages/results.php
@@ -8,19 +8,23 @@
         <?php 
             require(get_path('public/partials/global/header.php'));
         ?>
-        <main>
-            <h1>We found you some matches!</h1>
-            <p>Take your first step and select one of the mentors below.</p>
+        <main class="results">
             <section>
-                <?php foreach(array_slice($mentors, 0, 3) as $mentor): ?>
-                    <?php include(get_path('public/partials/components/mentor-card.php')); ?>
-                <?php endforeach; ?>
+                <h1>We found you some matches!</h1>
+                <p>Take your first step and select one of the mentors below.</p>
             </section>
-            <a href="#" class="btn">See More Matches</a>
-            <div>
+            <section>
+                <div class="matches">
+                    <?php foreach(array_slice($mentors, 0, 3) as $mentor): ?>
+                        <?php include(get_path('public/partials/components/mentor-card.php')); ?>
+                    <?php endforeach; ?>
+                </div>
+                <a href="#" class="btn">See More Matches</a>
+            </section>
+            <section class="d-flex opts">
                 <a href="#" title="Edit Answers">Edit Answers</a>
                 <a href="#" title="See all Mentors">See All Mentors</a>
-            </div>
+            </section>
         </main>
         <?php 
             require(get_path('public/partials/global/footer.php'));
