6cf025bd0e25534b6eb2ee938d535ad86eaf6bae
marlowe cheng <114950923+marlowecheng@users.noreply.github.com>
2023-03-11 17:59:48 -0800

    footer & header changes

--- public/partials/global/header.php
+++ public/partials/global/header.php
@@ -1,20 +1,22 @@
 <header>
-    <a href="<?php echo get_public_url('/index.php'); ?>" title="Home">
-        <img src="" alt="">
-    </a>
-    <nav>
-        <ul class="global-nav">
-            <li><a href="<?php echo get_public_url('/pages/mentor-list.php'); ?>" title="See all Mentors">Community Hub</a></li>
-            <li><a href="<?php echo get_public_url('/pages/project-showcase.php'); ?>" title="See Project Showcase">Mentors</a></li>
-            <li><a href="<?php echo get_public_url('/pages/project-showcase.php'); ?>" title="See Project Showcase">Notifications</a></li>
-            <li>
-                <div class="img-container">
-                <a href="<?php echo get_public_url('/pages/profile-page.php'); ?>" title="View User Profile"></a>
-                </div>
-            </li>
-            <li>
-                <a href="">User</a>
-            </li>
-        </ul>
-    </nav>
+    <div class="header-content">
+        <a href="<?php echo get_public_url('/index.php'); ?>" title="Home">
+            <img src="" alt="">
+        </a>
+        <nav>
+            <ul class="global-nav">
+                <li><a href="<?php echo get_public_url('/pages/mentor-list.php'); ?>" title="See all Mentors">Community Hub</a></li>
+                <li><a href="<?php echo get_public_url('/pages/project-showcase.php'); ?>" title="See Project Showcase">Mentors</a></li>
+                <li><a href="<?php echo get_public_url('/pages/project-showcase.php'); ?>" title="See Project Showcase">Notifications</a></li>
+                <li>
+                    <div class="img-container">
+                    <a href="<?php echo get_public_url('/pages/profile-page.php'); ?>" title="View User Profile"></a>
+                    </div>
+                </li>
+                <li>
+                    <a href="">User</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </header>
